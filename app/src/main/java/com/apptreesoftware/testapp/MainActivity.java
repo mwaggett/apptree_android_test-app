3dd0bbb28f6ce783b3e29367f220361910453586
Molly Waggett <mwaggett@alumni.reed.edu>
2015-11-12 15:40:49 -0800

    Add the ability to create a new person by tapping the FAB in the lower right corner.

--- app/src/main/java/com/apptreesoftware/testapp/MainActivity.java
+++ app/src/main/java/com/apptreesoftware/testapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.apptreesoftware.testapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +9,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     PeopleAdapter adapter;
+    ArrayList<Person> people;
+    FloatingActionButton fab;
+    RelativeLayout newPersonForm;
+    EditText firstNameText;
+    EditText lastNameText;
+    EditText phoneText;
+    EditText emailText;
+    EditText addressText;
+    Button saveButton;
+    Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +38,47 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         listView = (ListView) findViewById(R.id.listView);
+        people = ModelController.getInstance().getPeople();
         setupPeopleAdapter();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        newPersonForm = (RelativeLayout) findViewById(R.id.newPersonForm);
+        firstNameText = (EditText) findViewById(R.id.firstNameText);
+        lastNameText = (EditText) findViewById(R.id.lastNameText);
+        phoneText = (EditText) findViewById(R.id.phoneText);
+        emailText = (EditText) findViewById(R.id.emailText);
+        addressText = (EditText) findViewById(R.id.addressText);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                toggleVisibility();
+            }
+        });
 
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = people.size()+1;
+                String firstName = firstNameText.getText().toString();
+                String lastName = lastNameText.getText().toString();
+                String phoneNumber = phoneText.getText().toString();
+                String email = emailText.getText().toString();
+                String address = addressText.getText().toString();
+
+                Person newPerson = new Person(id, firstName, lastName, email, phoneNumber, address);
+                people.add(newPerson);
+
+                toggleVisibility();
+            }
+        });
+
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleVisibility();
             }
         });
     }
@@ -64,8 +115,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupPeopleAdapter() {
-        adapter = new PeopleAdapter(this,0, ModelController.getInstance().getPeople());
+        adapter = new PeopleAdapter(this,0, people);
         listView.setAdapter(adapter);
 
+    }
+
+    private void toggleVisibility() {
+        if (listView.getVisibility() == View.VISIBLE) {
+            listView.setVisibility(View.INVISIBLE);
+            fab.setVisibility(View.INVISIBLE);
+            newPersonForm.setVisibility(View.VISIBLE);
+        } else {
+            listView.setVisibility(View.VISIBLE);
+            fab.setVisibility(View.VISIBLE);
+            firstNameText.setText("");
+            lastNameText.setText("");
+            phoneText.setText("");
+            emailText.setText("");
+            addressText.setText("");
+            newPersonForm.setVisibility(View.INVISIBLE);
+        }
     }
 }
--- app/src/main/java/com/apptreesoftware/testapp/Person.java
+++ app/src/main/java/com/apptreesoftware/testapp/Person.java
@@ -16,4 +16,15 @@ public class Person {
     String address;
     Bitmap photo;
 
+    public Person() {}
+
+    public Person(int id, String firstName, String lastName, String email, String phoneNumber, String address) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
 }
